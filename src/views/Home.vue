3bdb04e05bf7223396488fa0a4e728e5090ee7ae
Maxime Moreillon <m.moreillon@gmail.com>
2021-12-07 09:39:27 +0900

    working prototype

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,106 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-card>
+
+
+    <v-carousel
+      v-if="images.length"
+      v-model="carrousel_index"
+      height="auto"
+      hide-delimiters>
+      <v-carousel-item
+
+        v-for="(image, index) in images"
+        :key="image._id">
+
+        <v-card>
+          <v-card-text>
+            <v-img
+              height="400px"
+              contain
+              :src="image_src(image)"
+              alt=""/>
+          </v-card-text>
+
+          <v-card-text>
+            <v-row justify="center">
+              <v-col
+                v-for="label in labels"
+                :key="`item_${image._id}_${label}`"
+                cols="auto">
+                <v-btn
+                  :color="button_color(image, label)"
+                  @click="annotate_item(image, label, index)">
+                  {{label}}
+                </v-btn>
+              </v-col>
+
+            </v-row>
+          </v-card-text>
+        </v-card>
+
+
+
+
+      </v-carousel-item>
+    </v-carousel>
+  </v-card>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    data(){
+      return {
+        carrousel_index: 0,
+        images: [],
+      }
+
+    },
+    mounted(){
+      this.get_images()
+    },
+    methods: {
+      image_src({_id}){
+        return `${process.env.VUE_APP_IMAGE_STORAGE_API_URL}/images/${_id}/image`
+      },
+      get_images(){
+        this.loading = true
+        const url = `${process.env.VUE_APP_IMAGE_STORAGE_API_URL}/images`
+        this.axios.get(url)
+        .then( ({data}) => { this.images = data })
+        .catch( (error) => {
+          console.error(error)
+        })
+        .finally( () => {
+          this.loading = false
+        })
+      },
+      annotate_item(item, annotation, index){
+        const url = `${process.env.VUE_APP_IMAGE_STORAGE_API_URL}/images/${item._id}`
+        this.axios.patch(url, {annotation})
+        .then( ({data}) => {
+          // DIRTY
+          this.$set(this.images,index,data)
+          this.carrousel_index ++
+        })
+        .catch( (error) => {
+          console.error(error)
+        })
+        .finally( () => {
+          this.loading = false
+        })
+      },
+      button_color(item, label){
+        if(!item.data || !item.data.annotation) return
+        if(item.data.annotation === label) return 'green'
+      }
+    },
+    computed: {
+      labels(){
+        return process.env.VUE_APP_LABELS.split(',')
+      }
+    }
+
   }
-}
 </script>
