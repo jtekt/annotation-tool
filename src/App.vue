de747d0f959a397f1fceaba6a669fb432cbc12f3
Maxime Moreillon <m.moreillon@gmail.com>
2023-02-01 12:41:58 +0900

    author

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,33 @@
 <template>
-  <AppTemplate
-    :options="options">
-
+  <AppTemplate :options="options">
     <template v-slot:nav>
-      <v-list
-        dense
-        nav >
+      <v-list dense nav>
         <v-list-item
           v-for="(item, index) in nav"
           :key="`nav_item_${index}`"
           :to="item.to"
-          exact>
+          exact
+        >
           <v-list-item-icon>
-            <v-icon>{{item.icon}}</v-icon>
+            <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
 
           <v-list-item-content>
-            <v-list-item-title>{{item.title}}</v-list-item-title>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </template>
-
   </AppTemplate>
 </template>
 
 <script>
-import AppTemplate from '@moreillon/vue_application_template_vuetify'
+import AppTemplate from "@moreillon/vue_application_template_vuetify"
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    AppTemplate
+    AppTemplate,
   },
 
   data: () => ({
@@ -39,17 +35,25 @@ export default {
       title: "Annotation tool",
       login_url: process.env.VUE_APP_LOGIN_URL,
       identification_url: process.env.VUE_APP_IDENTIFICATION_URL,
-      header_logo: require('@/assets/jtekt_logo_negative.jpg'),
-      authentication_logo: require('@/assets/jtekt_logo.jpg'),
-      colors: { app_bar: '#000' },
-      author: 'JTEKT Corporation',
+      header_logo: require("@/assets/jtekt_logo_negative.jpg"),
+      authentication_logo: require("@/assets/jtekt_logo.jpg"),
+      colors: { app_bar: "#000" },
+      author: "Maxime Moreillon, JTEKT Corporation",
     },
     nav: [
-      {title: 'Items', to: {name: 'items'}, icon: 'mdi-format-list-bulleted'},
-      {title: 'About', to: {name: 'About'}, icon: 'mdi-information-outline'},
-    ]
+      {
+        title: "Items",
+        to: { name: "items" },
+        icon: "mdi-format-list-bulleted",
+      },
+      {
+        title: "About",
+        to: { name: "About" },
+        icon: "mdi-information-outline",
+      },
+    ],
   }),
-};
+}
 </script>
 
 <style>
